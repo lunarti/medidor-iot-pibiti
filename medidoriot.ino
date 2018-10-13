7455b151b9ceaad7e49e7fcd3dd3fd602415c287
lunarti <44105110+lunarti@users.noreply.github.com>
2018-10-13 09:39:07 -0300

    Add files via upload

---
+++ medidoriot.ino
@@ -0,0 +1,372 @@
+#include <user_interface.h>;
+os_timer_t tmr0;//Cria o Timer. Maximo de 7 Timer's.
+os_timer_t tmr1;//Cria o Timer. Maximo de 7 Timer's.
+
+#include <DS3231.h>
+#include <Wire.h>
+RTClib RTC;
+DS3231 Clock;
+String data="";
+
+//#include "EmonLib.h"                        // inclui a biblioteca
+//EnergyMonitor emon1;                        // Cria uma instância
+//#define   SAMPLING_TIME     0.0001668649    // intervalo de amostragem 166,86us
+//#define   LINE_FREQUENCY    60              // frequencia 60Hz Brasil
+//#define   VOLTAGE_AC        127.00          // 127 Volts
+//#define   ACS_MPY           15.41           // ganho/calibracao da corrente
+//double Irms = 0;
+//double a2d = 0;
+
+int idamostra = 0;
+int idregistro = 0;
+int idbackup = 0;
+int offset= 0;
+int flagb=0;
+int flaga=0;
+int envia=0;
+double Voltage = 0;
+double tensaoplaca =0;
+double mediat=0;
+double mediac=0;
+double Current = 0;
+double Currente = 0;
+int pmode = 1;
+int idregbackup=0;
+int flag=0;
+double backup[650];
+String linex={};
+ 
+#include <ESP8266WiFi.h>
+const char* ssid  = "Valadao";
+const char* password = "valadao01";
+const char* host = "192.168.100.4";
+const char* passcode = "code";
+
+#include "MCP320X.h"
+// (Optional) Define model and SPI pins
+#define CS_PIN D4     // ESP8266 default SPI pins
+#define CLOCK_PIN D5  // Should work with any other GPIO pins, since the library does not formally
+#define MOSI_PIN D7   // use SPI, but rather performs pin bit banging to emulate SPI communication.
+#define MISO_PIN D6   //
+#define MCP3208 8     // (Generally "#define MCP320X X", where X is the last model digit/number of inputs)
+MCP320X adc(MCP3208, CLOCK_PIN, MOSI_PIN, MISO_PIN, CS_PIN);
+
+//1-8 - chan 0-7 -> the 8 levels to be measured
+//9 DGND -> GND
+//10 CS chip select -> D4
+//11 Din MOSI -> D7
+//12 Dout MISO -> D6
+//13 CLC clock -> D5
+//14 AGN -> GND
+//15 Vref -> reference voltage (that gives max adc reading)
+//16 Vdd -> supply voltage, max 5.5V so Arduino 5V is fine
+
+//1 | u | 16 
+//2 |   | 15 
+//3 |   | 14 
+//4 |   | 13 
+//5 |   | 12 
+//6 |   | 11 
+//7 |   | 10 
+//8 |___| 9 
+
+
+
+void enviowifi(){
+  
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+    
+  Serial.print("\n Connecting to ");
+  Serial.println(host);
+
+  WiFiClient client;
+  if (!client.connect(host, 80)) {
+    Serial.println("Connection failed!");
+    return;
+  }
+
+    
+    // Create a URL for the request. Modify YOUR_HOST_DIRECTORY so that you're pointing to the PHP file.
+    String url = "http://";
+    if(pmode==1){
+    url += host;
+    url += "/index.php?m=";
+    url += pmode;
+    url += "&p=";
+    url += passcode;
+    url += "&i=";
+    url += idregistro;//
+    url += "&c=";
+    url += Currente;
+    url += "&v=";
+    url += tensaoplaca;
+    url += "&d=";
+    url += data;
+    }
+    else if(pmode==3)
+    {
+    url += host;
+    url += "/index.php?m=";
+    url += pmode;
+    url += "&p=";
+    url += passcode;
+    url += "&qtdeamostra=";
+    url += idregbackup;
+    url += "&cmed=";
+    for(int i = offset; i <= (idregbackup*2); i=i+2){
+        url += backup[i];
+
+        if(offset==0)
+        {
+          if((i!=((idregbackup*2)-2)))
+          {url += ",";}
+          if(i==(idregbackup*2)-2)
+          {break;}
+        }
+        else if(i!=(offset-2))
+          {url += ",";}
+        if((i==(idregbackup*2)-2)&&(offset!=0))
+          {i=0;
+          flag=1;
+          url += backup[i];
+          if(i!=offset-2)
+          {url+=",";}}
+        if((i==offset-2)&&(flag==1))
+          {break;}
+      }
+    flag=0;
+    
+  url += "&vmed=";
+    for(int i = offset+1; i <= (idregbackup*2); i=i+2){
+        url+=backup[i];
+        if(offset==0)
+        {
+          if((i!=((idregbackup*2)-1)))
+          {url+=",";}
+          if(i==(idregbackup*2)-1)
+          {break;}
+        }
+        else if(i!=(offset-1))
+          {url+=",";}
+        if((i==(idregbackup*2)-1)&&(offset!=0))
+          {
+          i=1;
+          flag=1;
+          url+=backup[i];
+          if(i!=offset-1)
+          {url+=",";}
+          }
+        if((i==offset-1)&&(flag==1))
+          {break;}
+      }    
+    url += "&d=";
+    url += data;
+    flag=0;
+    pmode=1;
+    }
+
+  // This will send the request to the server
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+    }
+  }
+
+    // Read all the lines of the reply from server and print them to Serial
+
+  while(client.available()){
+    linex = client.readStringUntil('\r');
+    Serial.print(linex);
+  }  
+  Serial.print("\n");
+  Serial.print(linex.substring((linex.length()-2),(linex.length())));
+  Serial.print("\n");
+  if(linex.substring(linex.length()-2,linex.length())=="OK")
+  {
+    Serial.print("foi \n");
+    idbackup=0;
+    offset=0;
+    idregbackup=0;
+    flagb=0;
+  }
+
+}
+
+void corrente(){
+    Voltage=0;
+    for(int i = 0; i < 1000; i++) {
+    Voltage = (Voltage + (.00122 * adc.readADC(0))); // (5 V / 1024 (Analog) = 0.0049) which converter Measured analog input voltage to 5 V Range
+    delay(1);
+    }
+    Voltage = Voltage /1000;
+    Current = (Voltage -2.5)/ 0.185; // Sensed voltage is converter to current
+    Currente=Current;
+    Serial.print("\t Current (A) = "); // shows the voltage measured
+    Serial.print(Currente,2); // the ‘2’ after voltage allows you to display 2 digits after decimal point
+    Serial.print("\n");
+
+//    Irms = emon1.calcIrms(1996); //leitura AC do ACS712 
+}
+
+void tensao(){
+    Voltage=0;
+    for(int i = 0; i < 1000; i++) {
+    Voltage =(Voltage + (adc.readADC(1)/819.2));
+    delay(1);
+    }
+    Voltage = Voltage /1000;
+    tensaoplaca=Voltage*26.3;
+    Serial.print("\t tensaum (V) = "); // shows the voltage measured
+    Serial.print(tensaoplaca,2); // the ‘2’ after voltage allows you to display 2 digits after decimal point
+    Serial.print("\n");
+}
+
+void datanow(){
+    DateTime now = RTC.now();
+    data="";
+    data+=now.year();
+    data+="/";
+    data+=now.month();
+    data+="/";
+    data+=now.day();
+    data+=now.hour();
+    data+=":";
+    data+=now.minute();
+    data+=":";
+    data+=now.second();
+}
+
+
+//void amostrar() {
+//    tensao();
+//    corrente();
+//    mediat=mediat+tensaoplaca;
+//    mediac=mediac+Currente;
+//    idregistro++;
+//    if(idregistro>1)
+//    { 
+//      backup[idbackup]=(mediac/idregistro);
+//      backup[idbackup+1]=(mediat/idregistro);
+//      idregistro=0;
+//      if(idregbackup<3)//nro max de registro de backup, tamanho do vetor backup/2
+//        {idregbackup++;}
+//      idbackup=idbackup+2;
+//      mediat=0;
+//      mediac=0;
+//      if(flagb==1)
+//            { 
+//            offset=offset+2;
+//            if(offset==6)//tamanho do vetor backup
+//              {offset=0;} 
+//            }
+//      if(idbackup==6)//tamanho do vetor backup
+//            {flagb=1;
+//            idbackup=0;}
+//    }   
+//}
+
+void flagamostrar(void*z)
+{
+  flaga=1;
+  }
+
+  void flagenviar(void*z)
+{
+  envia=1;
+  }
+
+void setup() {
+  
+//  emon1.current(0, ACS_MPY);             // Corrente: pino analógico, calibracao.
+  Serial.begin(115200);
+  Wire.begin();
+
+//    Clock.setClockMode(false);  // set to 24h
+//    //setClockMode(true); // set to 12h
+//
+//    Clock.setYear(2018-48);
+//    Clock.setMonth(8);
+//    Clock.setDate(10);
+//    Clock.setDoW(6);
+//    Clock.setHour(19);
+//    Clock.setMinute(3);
+//    Clock.setSecond(0);
+
+    datanow();
+    Serial.print(data);
+  os_timer_setfn(&tmr0, flagamostrar, NULL); //Indica ao Timer qual sera sua Sub rotina.
+  os_timer_arm(&tmr0, 60000, true);  //Indica ao Timer seu Tempo em mS e se sera repetido ou apenas uma vez (loop = true)
+                                     //Neste caso, queremos que o processo seja repetido, entao usaremos TRUE.
+  
+  os_timer_setfn(&tmr1, flagenviar, NULL); 
+  os_timer_arm(&tmr1, 180000, true);  
+}
+
+void loop() {
+  tensao();
+  corrente();
+  mediat=mediat+tensaoplaca;
+  mediac=mediac+Currente;
+  idregistro++;
+  
+ if(flaga==1)
+  {  
+    flaga=0;
+    backup[idbackup]=(mediac/idregistro);
+    backup[idbackup+1]=(mediat/idregistro);
+    Serial.print("idregistro:");
+    Serial.print(idregistro);
+    Serial.print("\r\n");
+    idregistro=0;
+    if(idregbackup<300)//nro max de registro de backup, tamanho do vetor backup/2
+      {idregbackup++;}
+    idbackup=idbackup+2;
+    mediat=0;
+    mediac=0;
+    if(flagb==1)
+      { 
+      offset=offset+2;
+      if(offset==600)//tamanho do vetor backup
+        {offset=0;} 
+      }
+      if(idbackup==600)//tamanho do vetor backup
+        {flagb=1;
+        idbackup=0;}
+      if(idregbackup>2)
+        {
+          if(envia==1)
+          {
+          pmode=3;
+          datanow();
+          enviowifi();
+          envia=0;
+          }
+        }
+  
+  }
+
+    delay(5000);
+}
+
+
+
---
+++ requisicao.php
@@ -0,0 +1,164 @@
+<?php
+	/*
+		Description: PHP code for NodeMCU project which inserts the date and two switch statuses into a MySQL database table.
+		Create a table in a database with columns: "date" (text), "c" (boolean), "v" (boolean).
+		
+		Author: Matthew W. - www.mwhprojects.com - www.github.com/mwhprojects/NodeMCU-MySQL
+	*/
+	
+	/* This PHP file is used to insert values into the database as well as display them on a webpage for you to view. The following PHP code at the top of this file is to get and insert values into the database, if the parameters are set in the URL. The PHP code later on is to display the values on the page. */
+	// Get values.
+
+	$pass = $_GET['pass'];
+	$mode = $_GET['m'];
+	date_default_timezone_set('America/Sao_Paulo');
+	$dateserver= date('Y-m-d H:i:s') ;
+	$intervalotempobackup = 1;
+	//$formatostringdata = "2006/01/01 08:04:20";
+	//$formatorequisição = http://localhost/index.php?i=1&c=1.2&m=1&v=1.1&p=code&d=2008/08/06%2008:04:20
+	
+	// Set password. This is just to prevent some random person from inserting values. Must be consistent with YOUR_PASSCODE in Arduino code.
+	$passcode = "code";
+	
+	// database credentials
+	$servername = "localhost";
+	$username = "root";
+	$dbname = "pibiti";
+	$password = "";
+	
+	// Check if password is right. (If there is no password, assume no data is trying to be entered and skip over this.)
+	if(isset($pass) && ($pass == $passcode))
+	{
+		//verifica se o mode foi selecionado
+		if(isset($mode)) 
+		{			
+			if($mode==1) // idamostra, corrente, tensão, dataserver, dataamostra
+			{
+				//$date = date('Y-m-d H:i:s',strtotime("+$soma day",strtotime($_GET['d'])));
+				$date = date('Y-m-d H:i:s',strtotime($_GET['d']));
+				$idamostra = $_GET['i'];
+				$current = $_GET['c'];
+				$voltage = $_GET['v'];
+				$pass = $_GET['p'];
+				
+				if(isset($current)&&isset($voltage)&&isset($date)&&isset($idamostra))
+				{
+					$conn = mysqli_connect($servername, $username, $password, $dbname);
+					
+					if (!$conn) 
+					{
+						die("Connection failed: " . mysqli_connect_error());
+					}
+					
+					$sql = "INSERT INTO dadosp (date, dateserver, idamostra, current, voltage)
+					VALUES ('$date', '$dateserver','$idamostra', '$current', '$voltage')";
+					
+					if (mysqli_query($conn, $sql)) 
+					{
+						echo "OK DADOS GRAVADOS mode 1";
+					} 
+					else 
+					{
+						echo "Fail: " . $sql . "<br/>" . mysqli_error($conn);
+					}
+					// Close connection.
+					mysqli_close($conn);
+				}
+			}			
+			else if($mode==2) // temperaturas,idamostra, e data(data da página ou 1 valor do rtc e somar os pulos nas próximas temperaturas), envio por variação de estado
+			{
+				$date = date('Y-m-d H:i:s',strtotime($_GET['d']));
+				$idamostra = $_GET['i'];
+				$t1 = $_GET['t1'];
+				$t2 = $_GET['t2'];
+				$t3 = $_GET['t3'];
+				$t4 = $_GET['t4'];
+				$t5 = $_GET['t5'];
+				
+				if(isset($t1)&&isset($t2)&&isset($t3)&&isset($t4)&&isset($t5)&&isset($date)&&isset($idamostra))
+				{
+					$conn = mysqli_connect($servername, $username, $password, $dbname);
+					
+					if (!$conn) 
+					{
+						die("Connection failed: " . mysqli_connect_error());
+					}
+					
+					$sql = "INSERT INTO dadostemp (date, dateserver, idamostra, t1, t2, t3, t4, t5)
+					VALUES ('$date', '$dateserver','$idamostra', '$t1, '$t2','$t3','$t4','$t5')";
+					
+					if (mysqli_query($conn, $sql)) 
+					{
+						echo "OK DADOS GRAVADOS mode 2";
+					} 
+					else 
+					{
+						echo "Fail: " . $sql . "<br/>" . mysqli_error($conn);
+					}
+					// Close connection.
+					mysqli_close($conn);
+				}
+				
+			}
+			else if($mode==3) // entrada backup - 1 valor do rtc, depois um while inserindo os valores armazenados todos enviando em apenas 1 get(enviando um array) e 1 data
+			{
+				//http://localhost/index.php?pass=code&m=3&qtdeamostra=2&tmed=1,2&p=2,3&idamostra=1&d=2008/08/06%2008:04:20
+				$qtdeamostra = $_GET['qtdeamostra'];
+				$tmed = explode(",", $_GET["tmed"]);
+				$pot = explode(",", $_GET["p"]);
+				$idamostra = $_GET['idamostra'];
+				$date = date('Y-m-d H:i:s',strtotime($_GET['d']));
+				
+				if(count($tmed)!=$qtdeamostra&&count($pot)!=$qtdeamostra)
+				{
+					echo "Quantidade de amostrar não bate com tamanho de vetores";
+					echo  $qtdeamostra . " qtdeamostra " . count($tmed) . " counttmed " . count($pot) . " countpot <br/>";
+				}
+				else if(isset($tmed)&&isset($pot)&&isset($date)&&isset($qtdeamostra)&&isset($idamostra))
+				{
+					$conn = mysqli_connect($servername, $username, $password, $dbname);
+					
+					if (!$conn) 
+					{
+						die("Connection failed: " . mysqli_connect_error());
+					}
+					
+					for ($x = 0; $x < $qtdeamostra; $x++) 
+					{
+					$dateenv = date('Y-m-d H:i:s',strtotime("+$x minutes",strtotime($_GET['d'])));
+					$potenv = $pot[$x];
+					$tmedenv = $tmed[$x];
+					$sql = "INSERT INTO backup (idamostra, date, dateserver, pot, tmed)
+					VALUES ('$idamostra', '$dateenv', '$dateserver', '$potenv'	, '$tmedenv')";
+					
+						if (mysqli_query($conn, $sql)) 
+						{
+							echo "Envio: "
+							echo $x+1 . " de " . $qtdeamostra . "<br/>";
+						} 
+						else
+						{
+							echo "Fail: "
+							echo  $sql . "Valor enviado id:" . $x+1 . " de " . $qtdeamostra ."<br/>" . mysqli_error($conn);
+						}
+					} 
+
+					// Close connection.
+					mysqli_close($conn);
+				}
+				else
+					{
+						echo "fail g";
+					}
+					
+				
+			}
+			else if($mode==4) // variáveis de estado
+			{
+				
+			}	
+				
+		}
+	}
+	else{echo "Fail p";}
+?>
